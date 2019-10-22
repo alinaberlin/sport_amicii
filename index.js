f98f415242b2df9f4219f72b85eb690148551b2c
Alina Ghetler <alina.ghetler@gmail.com>
2019-10-22 21:07:07 +0200

    improve venue class

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ class User {
         this.location = location;
         this.sports = [];
     }
-    printName(){
-        console.log('Name:', this.firstName, this.secondName)
+    printName() {
+        console.log("Name:", this.firstName, this.secondName);
     }
     addInterest(sport) {
         this.sports.push(sport);
@@ -34,36 +34,44 @@ class SportEvent {
         this.participants = [];
     }
     printParticipantsNames() {
-        this.participants.forEach(p => p.printName())
-      }
+        this.participants.forEach(p => p.printName());
+    }
     addParticipants(participant) {
         if (!participant.sports.includes(this.sport)) {
-            return "Sport of this event is not in your interes ";
+            console.log("Sport of this event is not in your interes ");
+            return;
         }
         if (this.participants.length < this.maxNumber) {
             this.participants.push(participant);
-            return `Welcome ` + participant.firstName;
-        } else {
-            this.waitingList.push(participant);
+            console.log(`Welcome ` + participant.firstName);
+            return;
         }
+        this.waitingList.push(participant);
+        console.log("There are not empty spots, you're been added to waiting list");
     }
 }
 class Venue {
     constructor(name, address, sports) {
+        if (!Array.isArray(sports)){
+            throw 'Sports should be an array'//for solid code 
+        }
         this.name = name;
         this.address = address;
         this.sports = sports;
+    }
+    addSport(sport){
+        this.sports.push(sport)
     }
 }
 
 let running = new Sport("individual", 1);
 let hanball = new Sport("team", 6);
 let alina = new User("alina", "nasti", "alina.ghe@gmail.com");
-alina.addInterest(running)
+alina.addInterest(running);
 let aurel = new User("aurel", "nasti", "aurel.ghe@gmail.com");
-aurel.addInterest(running)
-let venue = new Venue("AlexanderPlatz", "Alex 24", "hanball");
+aurel.addInterest(running);
+let venue = new Venue("AlexanderPlatz", "Alex 24", [hanball, running]);
 let todayEven = new SportEvent(new Date(), venue, 6, running);
-console.log(todayEven.addParticipants(alina));
-console.log(todayEven.addParticipants(aurel));
-todayEven.printParticipantsNames()
+todayEven.addParticipants(alina);
+todayEven.addParticipants(aurel);
+todayEven.printParticipantsNames();
