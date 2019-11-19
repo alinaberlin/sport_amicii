8b210b5eae67258cd75e81e08a28f69ee69080a9
Alina Ghetler <alina.ghetler@gmail.com>
2019-11-19 07:14:16 +0100

    Add method add participant

--- routes/sport-event.js
+++ routes/sport-event.js
@@ -1,14 +1,18 @@
-const express = require('express')
-const router = express.Router()
-const SportEventService =require('../services/sport-event-service')
-
+const express = require("express");
+const router = express.Router();
+const SportEventService = require("../services/sport-event-service");
 
 router.post("/participate/:eventId/:userId", async (req, res) => {
     const user = await UserService.find(req.params.userId);
     const event = await SportEventService.find(req.params.eventId);
     console.log("Event is", event);
-    event.addParticipants(user);
+    const result = await SportEventService.addParticipant(user, event);
+    res.send(result);
+});
+
+router.post("/", async (req, res) => {
+    const event = await SportEventService.add(req.body);
     res.send(event);
 });
 
-module.exports = router
+module.exports = router;
--- services/sport-event-service.js
+++ services/sport-event-service.js
@@ -3,6 +3,11 @@ const SportEventModel = require("../models/sport");
 
 class SportEventService extends BaseService {
     model = SportEventModel;
+
+    async addParticipant(user, event) {
+        event.participants.push(user);
+        return await event.save();
+    }
 }
 
 module.exports = new SportEventService();
