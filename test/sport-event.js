7105172dc10a9c562b972c6e3275e92bbc369755
Alina Ghetler <alina.ghetler@gmail.com>
2019-11-26 12:30:36 +0100

    modified tests for sport event

--- test/sport-event.js
+++ test/sport-event.js
@@ -59,8 +59,6 @@ test("Add user to sport event", async t => {
         .send(sportEvent);
     t.is(res.status, 200);
     t.deepEqual(res.body.maxNumber, sportEvent.maxNumber);
-    
- 
 });
 test("Delete a sport event", async t => {
     t.plan(2);
@@ -80,16 +78,17 @@ test("Delete a sport event", async t => {
         venue: venue,
         sport: sport
     };
-    const createdSportEvent = (await request(app)
-        .post("/event")
-        .send(sportEvent)).body;
+    const createdSportEvent = (
+        await request(app)
+            .post("/event")
+            .send(sportEvent)
+    ).body;
     // delete the sport event
     const deleteRes = await request(app).delete(`/event/${createdSportEvent._id}`);
     // checking for server response status success
     t.is(deleteRes.status, 200);
     t.is(deleteRes.ok, true);
 });
-
 
 test("Create sport event", async t => {
     const sport = await SportService.add({
@@ -144,12 +143,8 @@ test("Add  new user to sport event", async t => {
         .send(sportEvent);
     t.is(res.status, 200);
     t.deepEqual(res.body.maxNumber, sportEvent.maxNumber);
-    
- 
 });
 test("Add 2 users to sport event", async t => {
-    t.plan(2);
-    // first create a sport event
     const sport = await SportService.add({
         name: "fotball",
         minAttendee: 6
@@ -159,21 +154,39 @@ test("Add 2 users to sport event", async t => {
         address: "Olympischer Platz 3, 14053 Berlin",
         sports: [sport]
     });
+    //creating the 2 users req for this test
+    const user = await UserService.add({
+        firstName: "Alina",
+        secondName: "Ghetler",
+        email: "alina.ghetler@gmail.com",
+        password: "dumy",
+        gender: "female",
+        location: "Berlin",
+        sports: [sport]
+    });
+    const user2 = await UserService.add({
+        firstName: "Alinus",
+        secondName: "Ghetler",
+        email: "alinus.ghetler@gmail.com",
+        password: "dumy",
+        gender: "female",
+        location: "Berlin",
+        sports: [sport]
+    });
     const sportEvent = {
         date: new Date(),
         maxNumber: 10,
         venue: venue,
         sport: sport
     };
-    const createdSportEvent = await SportEventService.add(sportEvent)
-    const res =await request(app)
-        .post("/event")
-        .send(sportEvent)
-    // delete the sport event
-    const res2 =await request(app)
-    .post("/event")
-    .send(sportEvent)
-    t.is(res.status, 200)
-    t.deepEqual(res2.body.maxNumber, sportEvent.maxNumber)
-});
-
+    const event = await SportEventService.add(sportEvent);
+    const res = await request(app)
+        .post(`/event/participate/${event._id}/${user._id}`)
+        .send(sportEvent);
+    const res2 = await request(app)
+        .post(`/event/participate/${event._id}/${user2._id}`)
+        .send(sportEvent);
+    t.is(res2.status, 200);
+    t.deepEqual(res2.body.maxNumber, sportEvent.maxNumber);
+ })
+ 
