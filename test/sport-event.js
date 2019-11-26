0ab3731b6434e16cf0a6e8a9887c6b49ecf0fbd9
Alina Ghetler <alina.ghetler@gmail.com>
2019-11-26 11:17:04 +0100

    adding new test at sport-event test

--- test/sport-event.js
+++ test/sport-event.js
@@ -89,3 +89,91 @@ test("Delete a sport event", async t => {
     t.is(deleteRes.status, 200);
     t.is(deleteRes.ok, true);
 });
+
+
+test("Create sport event", async t => {
+    const sport = await SportService.add({
+        name: "fotball",
+        minAttendee: 6
+    });
+    const venue = await VenueService.add({
+        name: "Olimpia Stadion",
+        address: "Olympischer Platz 3, 14053 Berlin",
+        sports: [sport]
+    });
+    const sportEvent = {
+        date: new Date(),
+        maxNumber: 10,
+        venue: venue,
+        sport: sport
+    };
+    const res = await request(app)
+        .post("/event")
+        .send(sportEvent);
+    t.is(res.status, 200);
+});
+
+test("Add  new user to sport event", async t => {
+    const sport = await SportService.add({
+        name: "fotball",
+        minAttendee: 6
+    });
+    const venue = await VenueService.add({
+        name: "Olimpia Stadion",
+        address: "Olympischer Platz 3, 14053 Berlin",
+        sports: [sport]
+    });
+    const user = await UserService.add({
+        firstName: "Alina",
+        secondName: "Ghetler",
+        email: "alina.ghetler@gmail.com",
+        password: "dumy",
+        gender: "female",
+        location: "Berlin",
+        sports: [sport]
+    });
+    const sportEvent = {
+        date: new Date(),
+        maxNumber: 10,
+        venue: venue,
+        sport: sport
+    };
+    const event = await SportEventService.add(sportEvent);
+    const res = await request(app)
+        .post(`/event/participate/${event._id}/${user._id}`)
+        .send(sportEvent);
+    t.is(res.status, 200);
+    t.deepEqual(res.body.maxNumber, sportEvent.maxNumber);
+    
+ 
+});
+test("Add 2 users to sport event", async t => {
+    t.plan(2);
+    // first create a sport event
+    const sport = await SportService.add({
+        name: "fotball",
+        minAttendee: 6
+    });
+    const venue = await VenueService.add({
+        name: "Olimpia Stadion",
+        address: "Olympischer Platz 3, 14053 Berlin",
+        sports: [sport]
+    });
+    const sportEvent = {
+        date: new Date(),
+        maxNumber: 10,
+        venue: venue,
+        sport: sport
+    };
+    const createdSportEvent = await SportEventService.add(sportEvent)
+    const res =await request(app)
+        .post("/event")
+        .send(sportEvent)
+    // delete the sport event
+    const res2 =await request(app)
+    .post("/event")
+    .send(sportEvent)
+    t.is(res.status, 200)
+    t.deepEqual(res2.body.maxNumber, sportEvent.maxNumber)
+});
+
